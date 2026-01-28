3a258b98b44132a7e03082b0270951c27b4dd91c
Max-Mukhiddin <@mukhiddinsolijonov101@gmail.com>
2026-01-28 11:51:24 +0900

    feat: SASS integration

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
   reactStrictMode: true,
 };
-
+module.exports = {
+  sassOptions: {
+    quietDeps: true,
+  },
+};
 export default nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-
 import { useState } from "react";
 import { light } from "@/scss/MaterialTheme";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState(createTheme(light));
